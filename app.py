447d4db126a7675a3dd201ea633c48445195f111
Rozag <www.rozag@gmail.com>
2024-05-17 20:27:22 +0700

    Read and write to Firestore, securely!

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 import streamlit as st
+from google.cloud import firestore
 
-st.header('Hello 🌎!')
-if st.button('Balloons?'):
-    st.balloons()
+# Authenticate to Firestore with the JSON account key.
+# db = firestore.Client.from_service_account_json("key.json")
+import json
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="streamlit-abdul-test-aam")
+
+# Create a reference to the Google post.
+doc_ref = db.collection("posts").document("Google")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
+data  = doc.to_dict()
+
+# Let's see what we got!
+for i in range(1,len(data)+1):
+  st.write(data[f"{i}"])
