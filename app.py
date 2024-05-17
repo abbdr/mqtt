5c023f56202a398f8b7c71265cd7ba253287fb9a
Rozag <www.rozag@gmail.com>
2024-05-18 01:02:18 +0700

    bismilah

--- app.py
+++ app.py
@@ -57,7 +57,6 @@ def subscribe(client: mqtt_client):
         # update operation (add new key value)
         doc_ref.update({f'{num}': message})
         num += 1
-        st.rerun()
 
         print(message)
         st.markdown(message)
