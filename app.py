1a8b51771484551817afdb758f8b77d966cdb420
Rozag <www.rozag@gmail.com>
2024-05-17 20:48:50 +0700

    p

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 import streamlit as st
 from google.cloud import firestore
+from google.oauth2 import service_account
 
 # Authenticate to Firestore with the JSON account key.
 # db = firestore.Client.from_service_account_json("key.json")
 import json
 key_dict = json.loads(st.secrets["textkey"])
 # db = firestore.Client.from_service_account_json(key_dict)
-# creds = service_account.Credentials.from_service_account_info(key_dict)
-# db = firestore.Client(credentials=creds, project="streamlit-abdul-test-aam")
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="streamlit-abdul-test-aam")
 
 # Create a reference to the Google post.
-# doc_ref = db.collection("posts").document("Google")
+doc_ref = db.collection("posts").document("Google")
 
 # Then get the data at that reference.
-# doc = doc_ref.get()
+doc = doc_ref.get()
 
-# data  = doc.to_dict()
+data  = doc.to_dict()
 
 # Let's see what we got!
-# for i in range(1,len(data)+1):
-#   st.write(data[f"{i}"])
+for i in range(1,len(data)+1):
+  st.write(data[f"{i}"])
 
-st.write(key_dict)
+# st.write(key_dict)
