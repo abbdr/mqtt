fa29474154bca70115ded1dafbce4ddee0ee1034
Rozag <www.rozag@gmail.com>
2024-05-17 21:13:45 +0700

    p

--- app.py
+++ app.py
@@ -4,23 +4,69 @@ from google.oauth2 import service_account
 import library.client as mqtt_client
 
 # Authenticate to Firestore with the JSON account key.
-# db = firestore.Client.from_service_account_json("key.json")
 import json
 key_dict = json.loads(st.secrets["textkey"])
-# db = firestore.Client.from_service_account_json(key_dict)
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="streamlit-abdul-test-aam")
 
-# Create a reference to the Google post.
-doc_ref = db.collection("posts").document("Google")
+# mqtt
+broker = 'broker.emqx.io'
+port = 1883
+topic = "alarm_anti_maling"
+# Generate a Client ID with the subscribe prefix.
+client_id = f'abdul-subscribe-test-python-{random.randint(0, 1000)}'
+# username = 'emqx'
+# password = 'public'
+# message = []
+num = 1
+data = []
 
-# Then get the data at that reference.
-doc = doc_ref.get()
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
 
-data  = doc.to_dict()
+    client = mqtt_client.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
 
-# Let's see what we got!
-for i in range(1,len(data)+1):
-  st.write(data[f"{i}"])
 
-# st.write(key_dict)
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        global num, data
+        message = f"{msg.payload.decode().upper()} &emsp; at `{ct}` "
+        # update operation (add new key value)
+        # if not db.reference(f"/{num}").get():
+        #     db.reference("/").update({num: message})
+        # num += 1
+
+        print(message)
+        st.markdown(message)
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    global num, data
+    client = connect_mqtt()
+    subscribe(client)
+    if 'file' not in st.session_state:
+        # Create a reference to the Google post.
+        doc_ref = db.collection("posts").document("Google")
+        # Then get the data at that reference.
+        doc = doc_ref.get()
+        data  = doc.to_dict()
+        # Let's see what we got!
+        for i in range(1,len(data)+1):
+          st.write(data[f"{i}"])
+        st.session_state.file = 0
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
